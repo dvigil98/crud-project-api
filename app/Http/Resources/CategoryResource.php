43f2ac2d09a31ea7608d7b1d94ae54d735bf24fd
David Vigil <vigilgarcia98@gmail.com>
2024-11-21 14:46:11 -0600

    Propiedad de descripcion de categoria en respuetsa json modificada

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -17,7 +17,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'descrption' => $this->description
+            'description' => $this->description
         ];
     }
 }
